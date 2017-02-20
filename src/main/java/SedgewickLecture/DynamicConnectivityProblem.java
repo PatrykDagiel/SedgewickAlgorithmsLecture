fccbaf4d067e343f5d5535c3eb3f4a3a75b039fe
Patryk Dagiel <patryk.dagiel@gmail.com>
2017-02-20 13:05:09 +0100

    StdLib added

--- src/main/java/SedgewickLecture/DynamicConnectivityProblem.java
+++
@@ -1,7 +0,0 @@
-package SedgewickLecture;
-
-/**
- * Created by dagiel on 20/02/2017.
- */
-public class DynamicConnectivityProblem {
-}
---
+++ src/main/java/SedgewickLecture/UF.java
@@ -0,0 +1,15 @@
+package SedgewickLecture;
+
+/**
+ * Created by dagiel on 20/02/2017.
+ */
+public class UF {
+    UF(int N) {};
+    void union(int p, int q) {};
+    boolean connected(int p, int q) {};
+
+    public static void main(String[] args) {
+
+    }
+
+}
